d9c2fb2c52393db30fe2f49728910c139eb4befd
johnkhub <johnkatanga777@gmail.com>
2023-08-14 08:50:15 +0200

    ok cool

--- src/main/java/gitpracb/GitPracBApplication.java
+++ src/main/java/gitpracb/GitPracBApplication.java
@@ -1,13 +1,25 @@
 package gitpracb;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//
 @SpringBootApplication
-public class GitPracBApplication {
+public class GitPracBApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(GitPracBApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Ok  psps");
+        System.out.println("Ok  psps");
+        System.out.println("Ok  psps");
+        System.out.println("Ok  psps");
+        System.out.println("Ok  psps");
+        System.out.println("Ok  psps");
+        System.out.println("Ok  psps");
+    }
 }
